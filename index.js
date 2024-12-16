aba1bdd4c4d793ae02e55e3b9bf52c07f8dee91d
tlesner <tjlesner@gmail.com>
2024-12-15 18:55:35 -0700

    challenge

---
+++ index.js
@@ -0,0 +1,76 @@
+// TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
+import generateMarkdown from './utils/generateMarkdown.js';
+
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input', 
+        message: 'Give your README.md a title!',
+        name: 'title',
+    },
+    {
+        type: 'input', 
+        message: 'Describe your project!',
+        name: 'description',
+    },
+    {
+        type: 'input', 
+        message: 'How can the end-user install this project?',
+        name: 'install',
+    },
+    {
+        type: 'input', 
+        message: 'What is the intended use of this application?',
+        name: 'usage',
+    },
+    {
+        type: 'input', 
+        message: 'What are your contribution guidelines for this app?',
+        name: 'guidelines',
+    },
+    {
+        type: 'input', 
+        message: 'Go the extra mile and write tests for your application. Then provide examples on how to run them here.',
+        name: 'test',
+    },
+    {
+        type: 'list', 
+        message: 'What license would you like to use?',
+        name: 'license',
+        choices: [ 'MIT', 'Apache License 2.0', 'The Unlicense']
+    },
+    {
+        type: 'input', 
+        message: 'What is your GitHub username?',
+        name: 'github',
+    },
+    {
+        type: 'input', 
+        message: 'What is your email?',
+        name: 'email',
+    },
+]
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+        err ? console.error(err) : console.log('Success!'))
+};
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer 
+        .prompt(questions)
+        .then((response) => {
+            const markdownData = generateMarkdown(response);
+            writeToFile('README.md', markdownData);
+        })
+        .catch((error) => console.log(error));
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,91 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {  
+  if (license === "MIT") {
+  let badge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  return badge;
+}
+else if (license === "Apache License 2.0") {
+  let badge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  return badge;
+}
+else if (license === "The Unlicense") {
+  let badge = "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+  return badge;
+}
+else (license === "")
+  return ""
+};
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    let link = "(https://opensource.org/licenses/MIT)";
+    return link;
+  }
+  else if (license === "Apache License 2.0") {
+    let link = "(https://opensource.org/licenses/Apache-2.0)"
+    return link;
+  }
+  else if (license === "The Unlicense") {
+    let link = "(http://unlicense.org/)"
+    return link;
+  }
+  else (license === "")
+    return ""
+  };
+
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  let licenseLink = renderLicenseLink(license);
+  let licenseName = license;
+
+  return `The application is covered under [${licenseName}](${licenseLink}).  
+  ${licenseLink}
+  `
+
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `
+# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+  ${data.description}
+
+## Table of Contents
+  1. [Installation Guide](#installation-guide)
+  2. [Usage](#usage)
+  3. [Contributing](#contributing)
+  4. [Tests](#tests)
+  5. [License](#license)
+  6. [Questions](#questions)
+
+## Installation Guide
+  ${data.install}
+
+## Usage 
+  ${data.usage}
+
+## Contributing
+  ${data.guidelines}
+
+## Tests
+  ${data.test}
+
+## License
+  ${renderLicenseSection(data.license)}
+
+## Questions
+  - []${data.github}
+  - You can reach me at ${data.email}
+`;
+}
+
+export default generateMarkdown;
